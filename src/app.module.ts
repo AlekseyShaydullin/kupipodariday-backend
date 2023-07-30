9591d3b6e583ae39dd755490ca560b542f347a5e
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-07-31 01:45:47 +0300

    feat: app structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+import { OffersModule } from './offers/offers.module';
 
 @Module({
   imports: [
@@ -11,13 +15,17 @@ import { AppController } from './app.controller';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.PORT) || 5432,
+      port: parseInt(process.env.DB_PORT) || 5432,
       username: process.env.DB_USER || 'student',
       password: process.env.DB_PASS || 'student',
       database: process.env.DB_NAME || 'kupipodariday',
       entities: [],
       synchronize: false,
     }),
+    UsersModule,
+    WishesModule,
+    WishlistsModule,
+    OffersModule,
   ],
   controllers: [AppController],
   providers: [],
