53599e15dfd74fc2cb847757ed0f2f6206e45de6
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-07-31 01:27:59 +0300

    feat: database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.PORT) || 5432,
+      username: process.env.DB_USER || 'student',
+      password: process.env.DB_PASS || 'student',
+      database: process.env.DB_NAME || 'kupipodariday',
+      entities: [],
+      synchronize: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [],
 })
