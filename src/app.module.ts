bfe0778a4976dff97456e23fa3f4afd1b4d84a09
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2023-08-16 01:30:01 +0300

    fixbugs: configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,25 +8,18 @@ import { WishlistsModule } from './wishlists/wishlists.module';
 import { OffersModule } from './offers/offers.module';
 import { AuthModule } from './auth/auth.module';
 import { HashModule } from './hash/hash.module';
-import { User } from './users/entities/user.entity';
-import { Offer } from './offers/entities/offer.entity';
-import { Wish } from './wishes/entities/wish.entity';
-import { Wishlist } from './wishlists/entities/wishlist.entity';
+import { config } from './configs/config';
+import { DatabaseConfig } from './configs/database.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      load: [config],
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT) || 5432,
-      username: process.env.DB_USER || 'student',
-      password: process.env.DB_PASS || 'student',
-      database: process.env.DB_NAME || 'kupipodariday',
-      entities: [User, Offer, Wish, Wishlist],
-      synchronize: true,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: DatabaseConfig,
     }),
     UsersModule,
     WishesModule,
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const port = configService.get('port') || 3000;
 
   const corsOptions: CorsOptions = {
     origin: 'http://localhost:3001',
@@ -14,7 +17,7 @@ async function bootstrap() {
   app.enableCors(corsOptions);
 
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000, () => {
+  await app.listen(port, () => {
     console.log(`App listening on port 3000`);
   });
 }
